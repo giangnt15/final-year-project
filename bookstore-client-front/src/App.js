75680c880d60bed3821dd3b133574fda60473f97
Nguyen Truong Giang <aquarius123654@outlook.com>
2020-01-09 17:31:54 +0700

    Sua

--- bookstore-client-front/src/App.js
+++ bookstore-client-front/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Header from './components/shared/headers/Header';
 import './App.css';
 import './components/shared/headers/headers.css';
@@ -6,29 +6,20 @@ import './css/style.css';
 import 'antd/dist/antd.css';
 import {Carousel} from 'antd';
 import MainSlider from './components/shared/slider/MainSlider';
+import SearchBox from './components/shared/search/SearchBox';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-	    <div className="wrapper" id="wrapper">
-        <Header />
-        <Carousel autoplay>
-    <div>
-      <h3>1</h3>
-    </div>
-    <div>
-      <h3>2</h3>
-    </div>
-    <div>
-      <h3>3</h3>
-    </div>
-    <div>
-      <h3>4</h3>
-    </div>
-  </Carousel>,
-      </div>
-    </div>
+    <Fragment>
+    
+      <Header />
+      <SearchBox />
+      <MainSlider />
+    </Fragment>
+       
   );
+}
 }
 
 export default App;
