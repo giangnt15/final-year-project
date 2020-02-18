54374d6c2230ab663910dc538ce8dce25764e5df
nguyen giang <aquarius123654@outlook.com>
2020-02-18 23:23:11 +0700

    Thêm dialog preview cho sách

--- bookstore-client-front/src/components/products/ModalProduct.js
+++ bookstore-client-front/src/components/products/ModalProduct.js
@@ -1,74 +1,13 @@
 import React from 'react';
+import {Popover, Button} from 'antd';
 
-function ModalProduct(props){
+function ModalProduct(props) {
 
-    return( <div className="modal-product">
-    {/* Start product images */}
-    <div className="product-images">
-      <div className="main-image images">
-        <img alt="big images" src="/images/product/big-img/1.jpg" />
-      </div>
-    </div>
-    {/* end product images */}
-    <div className="product-info">
-      <h1>Simple Fabric Bags</h1>
-      <div className="rating__and__review">
-        <ul className="rating">
-          <li><span className="ti-star" /></li>
-          <li><span className="ti-star" /></li>
-          <li><span className="ti-star" /></li>
-          <li><span className="ti-star" /></li>
-          <li><span className="ti-star" /></li>
-        </ul>
-        <div className="review">
-          <a href="#">4 customer reviews</a>
-        </div>
-      </div>
-      <div className="price-box-3">
-        <div className="s-price-box">
-          <span className="new-price">$17.20</span>
-          <span className="old-price">$45.00</span>
-        </div>
-      </div>
-      <div className="quick-desc">
-        Designed for simplicity and made from high quality materials. Its sleek geometry and material combinations creates a modern look.
-      </div>
-      <div className="select__color">
-        <h2>Select color</h2>
-        <ul className="color__list">
-          <li className="red"><a title="Red" href="#"></a></li>
-          <li className="gold"><a title="Gold" href="#"></a></li>
-          <li className="orange"><a title="Orange" href="#"></a></li>
-          <li className="orange"><a title="Orange" href="#"></a></li>
-        </ul>
-      </div>
-      <div className="select__size">
-        <h2>Select size</h2>
-        <ul className="color__list">
-          <li className="l__size"><a title="L" href="#">L</a></li>
-          <li className="m__size"><a title="M" href="#">M</a></li>
-          <li className="s__size"><a title="S" href="#">S</a></li>
-          <li className="xl__size"><a title="XL" href="#">XL</a></li>
-          <li className="xxl__size"><a title="XXL" href="#">XXL</a></li>
-        </ul>
-      </div>
-      <div className="social-sharing">
-        <div className="widget widget_socialsharing_widget">
-          <h3 className="widget-title-modal">Share this product</h3>
-          <ul className="social__net social__net--2 d-flex justify-content-start">
-            <li className="facebook"><a href="#" className="rss social-icon"><i className="zmdi zmdi-rss" /></a></li>
-            <li className="linkedin"><a href="#" className="linkedin social-icon"><i className="zmdi zmdi-linkedin" /></a></li>
-            <li className="pinterest"><a href="#" className="pinterest social-icon"><i className="zmdi zmdi-pinterest" /></a></li>
-            <li className="tumblr"><a href="#" className="tumblr social-icon"><i className="zmdi zmdi-tumblr" /></a></li>
-          </ul>
-        </div>
-      </div>
-      <div className="addtocart-btn">
-        <a href="#">Add to cart</a>
-      </div>
-    </div>
-  </div>);
+  return (
+    <Popover content={'content'} title="Title">
+      <Button type="primary">Hover me</Button>
+    </Popover> )
 
 }
 
-export {ModalProduct as default}
+export { ModalProduct as default }
--- bookstore-client-front/src/components/products/ProductItem.js
+++ bookstore-client-front/src/components/products/ProductItem.js
@@ -1,48 +1,120 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './products.css';
 import { NavLink } from 'react-router-dom';
+import { Popover } from 'antd';
+import Truncate from 'react-truncate';
 
 function ProductItem(props) {
-  const { id,thumbnail, basePrice, title } = props.book;
+  const { id, thumbnail, basePrice, title, authors, description } = props.book;
   const { width, thumbHeight } = props;
-  return (<div className="product product__style--3" style={{ width }}>
-    <div className="col-12">
-    {/* <div className="product__thumb" style={{height: 380}}> */}
-      <div className="product__thumb" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: thumbHeight }}>
-        <NavLink className="first__img" to={`/book/${id}`}><img src={thumbnail} alt="product image" /></NavLink>
-        {/* <a className="second__img animation1" href="single-product.html"><img src={thumbnail} alt="product image" /></a> */}
-        <div className="hot__box">
-          <span className="hot-label">BEST SALLER</span>
+  const productDialog = (<div id="express-buy-dialog" className="express-buy-l" >
+    <div className="loading" style={{}} />
+    <div className="content" id={105165} ng-show="productItem">
+      <div className="t-view ng-binding">
+        {title}
+      </div>
+      <div className="au-view">
+        {/* ngRepeat: item in productItem.Authors */}
+        {authors.map((item, index) => (
+          <Fragment key={item.id} ><NavLink to="#">{item.pseudonym}</NavLink>{index !== authors.length - 1 && ','} </Fragment>
+        ))}
+        {/* <a href="/tac-gia/chu-viet-nga-auth43519/p1" ng-repeat="item in productItem.Authors" className="ng-binding ng-scope">{}</a>end ngRepeat: item in productItem.Authors */}
+      </div>
+      <div className="des-view ng-binding">
+        <div class="product__info__main">
+          <div class="product__overview">
+            <div class="product_overview_content" style={{height: 200}} dangerouslySetInnerHTML={{ __html: description }}></div>
+            <div class="fade-footer"><NavLink to={`/book/${id}`} class="read-more text-primary font-weight-bold">Xem thêm</NavLink></div></div>
         </div>
       </div>
-      <div className="product__content content--center">
-        <h4><NavLink to={`/book/${id}`}>{title}</NavLink></h4>
-        <ul className="prize d-flex">
-          <li>${basePrice}</li>
-          <li className="old_prize">$35.00</li>
-        </ul>
-        <div className="action">
-          <div className="actions_inner">
-            <ul className="add_to_links">
-              <li><a className="cart" href="cart.html"><i className="bi bi-shopping-bag4" /></a></li>
-              <li><a className="wishlist" href="wishlist.html"><i className="bi bi-shopping-cart-full" /></a></li>
-              <li><a className="compare" href="#"><i className="bi bi-heart-beat" /></a></li>
-              <li><a data-toggle="modal" title="Quick View" className="quickview modal-view detail-link" href="#productmodal"><i className="bi bi-search" /></a></li>
+      <div className="p-view">
+        <span className="real-price ng-binding">67,000₫</span> &nbsp;&nbsp;
+      <span className="price ng-binding" ng-show="productItem.DiscountPercent>0">79,000₫</span>
+        <div className="discount-percent" ng-show="productItem.DiscountPercent>0">
+          <label> Giảm giá </label>
+          <span className="ng-binding">16 %</span>
+        </div>
+        <div className="clearfix" />
+      </div>
+      {/* <div className="gift-view ng-hide" ng-show="productItem.HasGift">
+      <div className="l">
+        <i className="fa fa-2x fa-gift" />
+      </div>
+      <div className="r ng-binding">
+      </div>
+      <div className="clearfix" />
+    </div> */}
+      <div className="vloader express-loading" style={{ display: 'none' }} />
+      <form className="ng-pristine ng-valid">
+        <input type="hidden" name="productid" className="productid" defaultValue={105165} />
+        <input type="hidden" name="Sizes" className="Sizes" defaultValue />
+        <input type="hidden" name="colorid" className="colorid" defaultValue />
+        <input type="hidden" name="fussedid" className="fussedid" defaultValue />
+        <input type="hidden" name="quantity" defaultValue={1} />
+        <input type="hidden" name="price" defaultValue={67000} />
+        <input type="hidden" name="actiontype" id="actiontype" defaultValue />
+        <input type="submit" className="btn-buy btn btn-bb add-to-cart" value="THÊM VÀO GIỎ HÀNG" />
+        {/* <div className="pre-box ng-hide" ng-show="productItem.IsNotPublished && productItem.QuantityRemain==null && !productItem.IsOutOff">
+        <small className="ng-binding">Sách này sắp phát hành </small>
+        <br />
+        <input type="submit" className="btn-pre btn btn-primary buy-now" defaultValue="Đặt hàng trước" />
+      </div> */}
+        {/* <div className="request-box ng-hide" ng-show="productItem.IsOutOff ">
+        <small>Hàng này không còn </small>
+        <br />
+        <a className="btn btn-request btn-danger">
+          <i className="fa fa-bullhorn" /> Báo tôi khi có hàng
+        </a>
+      </div> */}
+      </form>
+      <form action="/BookBuy/AddToFavorite" data-ajax-begin="AddToFavBegin" data-ajax-success="AddToFavSuccess" data-ajax="true" method="post" className="ng-pristine ng-valid">
+        <input type="hidden" name="productid" className="productid" defaultValue={105165} />
+        <a className="btn btn-default btn-fav">
+          <i className="fa fa-heart" />
+          Thêm vào yêu thích
+      </a>
+      </form>
+    </div>
+  </div>)
+  return (
+    <Popover content={productDialog} placement="left"> <div className="product product__style--3" style={{ width }}>
+      <div className="col-12">
+        {/* <div className="product__thumb" style={{height: 380}}> */}
+        <div className="product__thumb" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: thumbHeight }}>
+          <NavLink className="first__img" to={`/book/${id}`}><img src={thumbnail} alt="product image" /></NavLink>
+          {/* <a className="second__img animation1" href="single-product.html"><img src={thumbnail} alt="product image" /></a> */}
+          <div className="hot__box">
+            <span className="hot-label">BEST SALLER</span>
+          </div>
+        </div>
+        <div className="product__content content--center">
+          <h4><NavLink to={`/book/${id}`}>{title}</NavLink></h4>
+          <ul className="prize d-flex">
+            <li>${basePrice}</li>
+            <li className="old_prize">$35.00</li>
+          </ul>
+          <div className="action">
+            <div className="actions_inner">
+              <ul className="add_to_links">
+                <li><a className="cart" href="cart.html"><i className="bi bi-shopping-bag4" /></a></li>
+                <li><a className="wishlist" href="wishlist.html"><i className="bi bi-shopping-cart-full" /></a></li>
+                <li><a className="compare" href="#"><i className="bi bi-heart-beat" /></a></li>
+                <li><a data-toggle="modal" title="Quick View" className="quickview modal-view detail-link" href="#productmodal"><i className="bi bi-search" /></a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="product__hover--content">
+            <ul className="rating d-flex">
+              <li className="on"><i className="fa fa-star-o" /></li>
+              <li className="on"><i className="fa fa-star-o" /></li>
+              <li className="on"><i className="fa fa-star-o" /></li>
+              <li><i className="fa fa-star-o" /></li>
+              <li><i className="fa fa-star-o" /></li>
             </ul>
           </div>
         </div>
-        <div className="product__hover--content">
-          <ul className="rating d-flex">
-            <li className="on"><i className="fa fa-star-o" /></li>
-            <li className="on"><i className="fa fa-star-o" /></li>
-            <li className="on"><i className="fa fa-star-o" /></li>
-            <li><i className="fa fa-star-o" /></li>
-            <li><i className="fa fa-star-o" /></li>
-          </ul>
-        </div>
       </div>
-    </div>
-  </div>)
+    </div></Popover>)
 }
 
 export default ProductItem;
