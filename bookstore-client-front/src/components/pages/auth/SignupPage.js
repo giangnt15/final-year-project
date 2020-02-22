55c1e30a54587b262cd374e9dc1243bc621c86b5
Nguyen Truong Giang <aquarius123654@outlook.com>
2020-02-22 09:24:22 +0700

    Thêm chức năng đăng ký

--- bookstore-client-front/src/components/pages/auth/SignupPage.js
+++ bookstore-client-front/src/components/pages/auth/SignupPage.js
@@ -1,77 +1,159 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink, Redirect } from 'react-router-dom';
 import './main.css';
 import './util.css';
+import { signUp as signUpAction } from '../../../redux/actions/authAction';
+import { connect } from 'react-redux';
+import { withApollo } from '@apollo/react-hoc';
+import validator from 'validator';
+import { Button } from 'antd';
 
-function SignupPage(props){
+function SignupPage(props) {
 
-    return(<div>
-        <div className="limiter">
-          <div className="container-login100" style={{backgroundImage: 'url("https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80")'}}>
-            <div className="wrap-login100 p-l-70 p-r-70 p-t-50 p-b-33">
-              <div className="d-flex justify-content-center p-b-20">
-                  <NavLink to="/"><img src="/images/logo/logo.png" height={50}/></NavLink> 
-                  </div>
-              <form className="login100-form validate-form flex-sb flex-w">
-                <span className="login100-form-title p-b-20">
-                  Đăng ký
-                </span>
-                <div className="p-t-20 p-b-9">
-                  <span className="txt1">
-                    Email
-                  </span>
-                </div>
-                <div className="wrap-input100 validate-input" data-validate="Username is required">
-                  <input className="input100" type="email" name="email" />
-                  <span className="focus-input100" />
-                </div>
-                <div className="p-t-20 p-b-9">
-                  <span className="txt1">
-                    Tên người dùng
-                  </span>
-                </div>
-                <div className="wrap-input100 validate-input" data-validate="Username is required">
-                  <input className="input100" type="text" name="username" />
-                  <span className="focus-input100" />
-                </div>
-                <div className="p-t-13 p-b-9">
-                  <span className="txt1">
-                    Mật khẩu
-                  </span>
-                </div>
-                <div className="wrap-input100 validate-input" data-validate="Password is required">
-                  <input className="input100" type="password" name="pass" />
-                  <span className="focus-input100" />
-                </div>
-                <div className="p-t-13 p-b-9">
-                  <span className="txt1">
-                    Xác nhận khẩu
-                  </span>
-                </div>
-                <div className="wrap-input100 validate-input" data-validate="Password is required">
-                  <input className="input100" type="password" name="passConfirmation" />
-                  <span className="focus-input100" />
-                </div>
-                <div className="container-login100-form-btn m-t-17">
-                  <button className="login100-form-btn">
-                    Đăng ký
-                  </button>
-                </div> 
-                <div className="w-full text-center p-t-55">
-                  <span className="txt2 p-r-4">
-                    Đã có tài khoản?
-                  </span>
-                  <NavLink to="/auth/login" className="txt2 bo1">
-                    Đăng nhập
-                  </NavLink>
-                </div>
-              </form>
-            </div>
+  const { auth, signUp } = props;
+
+  const [inputs, setInputs] = useState({
+    username: '',
+    password: '',
+    email: '',
+    confirmPassword: ''
+  });
+
+  const [inputChanged, setInputChanged] = useState({
+    username: false,
+    password: false,
+    email: false,
+    confirmPassword: false
+  })
+
+  const handleInputsChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+    setInputs(prev => ({
+      ...prev,
+      [name]: value
+    }));
+    setInputChanged(prev => ({
+      ...prev,
+      [name]: true
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { username, password, email, confirmPassword } = inputs;
+    if (!username || !password || !email || !confirmPassword || password !== confirmPassword ||
+      !validator.isEmail(email)) {
+      setInputChanged({
+        username: true,
+        password: true,
+        email: true,
+      })
+      return;
+    }
+    signUp(email, username, password);
+  }
+
+  if (auth.token){
+    return <Redirect to="/" />
+  }
+
+  return (<div>
+    <div className="limiter">
+      <div className="container-login100" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80")' }}>
+        <div className="wrap-login100 p-l-70 p-r-70 p-t-50 p-b-33">
+          <div className="d-flex justify-content-center p-b-20">
+            <NavLink to="/"><img src="/images/logo/logo.png" height={50} /></NavLink>
           </div>
+          <form className="login100-form validate-form flex-sb flex-w" onSubmit={onSubmit}>
+            <span className="login100-form-title p-b-20">
+              Đăng ký
+                </span>
+            <div className="p-t-20 p-b-9">
+              <span className="txt1">
+                Email
+                  </span>
+            </div>
+            <div className="w-100">
+              <div className="wrap-input100 validate-input" data-validate="Username is required">
+                <input className="input100" type="email" name="email" onChange={handleInputsChange} value={inputs.email} />
+                <span className={`focus-input100${inputChanged.email && (!inputs.email || !validator.isEmail(inputs.email)) ? ' error' : ''}`} />
+              </div>
+              {inputChanged.email && !inputs.email && <span className="error-txt">Email không được để trống</span>}
+              {inputChanged.email && inputs.email && validator.isEmail(inputs.email) === false && <span className="error-txt">Email không đúng định dạng</span>}
+            </div>
+            <div className="p-t-20 p-b-9">
+              <span className="txt1">
+                Tên người dùng
+                  </span>
+            </div>
+            <div className="w-100">
+              <div className="wrap-input100 validate-input" data-validate="Username is required">
+                <input className="input100" type="text" name="username" onChange={handleInputsChange} value={inputs.username} />
+                <span className={`focus-input100${inputChanged.username && inputs.username ? '' : ' error'}`} />
+              </div>
+              {inputChanged.username && !inputs.username && <span className="error-txt">Username không được để trống</span>}
+            </div>
+            <div className="p-t-13 p-b-9">
+              <span className="txt1">
+                Mật khẩu
+                  </span>
+            </div>
+            <div className="w-100">
+              <div className={`wrap-input100 validate-input`} data-validate="Password is required">
+                <input className={`input100`} type="password" name="password"
+                  onChange={handleInputsChange} value={inputs.password} />
+                <span className={`focus-input100${inputChanged.password && inputs.password ? '' : ' error'}`} />
+              </div>
+              {inputChanged.password && !inputs.password && <span className="error-txt">Password không được để trống</span>}
+            </div>
+            <div className="p-t-13 p-b-9">
+              <span className="txt1">
+                Xác nhận khẩu
+                  </span>
+            </div>
+            <div className="w-100">
+              <div className={`wrap-input100 validate-input`} data-validate="Password is required">
+                <input className={`input100`} type="password" name="confirmPassword"
+                  onChange={handleInputsChange} value={inputs.confirmPassword} />
+                <span className={`focus-input100${inputChanged.confirmPassword && inputs.confirmPassword && inputs.password && inputs.confirmPassword !== inputs.password ? ' error' : ''}`} />
+              </div>
+              {inputChanged.confirmPassword && inputs.confirmPassword && inputs.password && inputs.confirmPassword !== inputs.password && <span className="error-txt">Xác nhận mật khẩu không trùng khớp</span>}
+            </div>
+            <div className="container-login100-form-btn m-t-17">
+              <Button htmlType="submit" type="primary" loading={auth.loading} className="login100-form-btn">
+                Đăng nhập
+            </Button>
+            </div>
+            <div className="w-full text-center p-t-55">
+              <span className="txt2 p-r-4">
+                Đã có tài khoản?
+                  </span>
+              <NavLink to="/auth/login" className="txt2 bo1">
+                Đăng nhập
+                  </NavLink>
+            </div>
+          </form>
         </div>
-        <div id="dropDownSelect1" />
-      </div>)
+      </div>
+    </div>
+    <div id="dropDownSelect1" />
+  </div>)
 
 }
 
-export default SignupPage;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    signUp: (email, username, password) => {
+      dispatch(signUpAction(ownProps.client, { email, username, password }));
+    }
+  }
+}
+
+export default withApollo(connect(mapStateToProps, mapDispatchToProps)(SignupPage));
