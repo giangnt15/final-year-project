dde79e122f50982dcca56517da6b8b017aace6f0
Nguyen Truong Giang <aquarius123654@outlook.com>
2020-02-27 16:52:45 +0700

    Sua AccountPage.js

--- bookstore-client-front/src/components/pages/auth/AccountPage.js
+++ bookstore-client-front/src/components/pages/auth/AccountPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Input, DatePicker, Radio, Button, Checkbox, Switch, Upload } from 'antd';
+import { Input, DatePicker, Radio, Button, Checkbox, Switch, Upload, Tabs } from 'antd';
 import { DATE_VN } from '../../../constants';
+
+const { TabPane } = Tabs;
 
 function AccountPage(props) {
 
@@ -8,24 +10,13 @@ function AccountPage(props) {
         <div className="p-t-120">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-3 col-12 order-2 order-lg-1 md-mt-40 sm-mt-40">
-                        <div className="shop__sidebar">
-                            <aside className="wedget__categories poroduct--cat">
-                                <h3 className="wedget__title">Tài khoản</h3>
-                                <ul>
-                                    <li><a href="#">Thông tin tài khoản</a></li>
-                                </ul>
-                            </aside>
-                        </div>
-                    </div>
-                    <div className="col-lg-9 col-12 order-1 order-lg-2" style={{ border: '1px solid #D3CED2' }}>
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="p-t-30 p-l-40 p-r-20">
+                    <Tabs defaultActiveKey="1" tabPosition={'top'} style={{maxHeight: 600}}>
+                        {[...Array(30).keys()].map(i => (
+                            <TabPane tab={`Thông tin tài khoản`} key={i} style={{padding: '0 0 50px 0'}}>
+                            <div className="p-l-80 p-r-80 p-t-30">
                                     <h4>Thông tin tài khoản</h4>
                                     <br />
-                                        <Upload className='avatar-uploader' style={{width: 100, height: 100}}/>
-                                    <form className="d-flex flex-column" style={{ width: 500 }} noValidate autoComplete="off">
+                                    <form className="d-flex flex-column" style={{ width: '80%' }} noValidate autoComplete="off">
                                         <div className="p-b-8"><label htmlFor="fullName">Họ và tên</label>
                                             <Input type="text" placeholder="Họ và tên" name="fullName" id="fullName" /></div>
                                         <div className="p-b-8"><label htmlFor="email">Email</label>
@@ -41,12 +32,12 @@ function AccountPage(props) {
                                             <Switch name="changePassword" id="changePassword" />
                                             <label htmlFor="changePassword" className="p-l-8">Đổi mật khẩu</label>
                                         </div>
-                                        <Button htmlType="submit" className="w-25" type="primary">Lưu thay đổi</Button>
+                                        <Button htmlType="submit" className="w-50" type="primary">Lưu thay đổi</Button>
                                     </form>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
+                            </TabPane>
+                        ))}
+                    </Tabs>
                 </div>
             </div>
         </div>
